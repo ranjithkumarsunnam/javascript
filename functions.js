a742f10f4c492dbf82d1c4dc65504ef725503311
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-12 19:38:43 +0530

    java script practice functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+//Normal function
+function SayHello(name){
+    console.log("Hello "+name);
+}
+SayHello("Ranjith Kumar");
+
+
+//Arrow Function
+const greet=(name)=>{
+    console.log("Greetings "+name);
+
+}
+greet("Ranjith Kumar");
+
+
+let personOne ={
+    name:'ranjith kumar',
+    age:'28'
+};
