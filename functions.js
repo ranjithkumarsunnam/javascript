37b2211fbd109c03ce1705a786f6521f63d2c83a
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-13 17:13:28 +0530

    functions,object,JSON, js for Backend practice

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-//Normal function
+/* //Normal function
 function SayHello(name){
     console.log("Hello "+name);
 }
@@ -14,6 +14,25 @@ greet("Ranjith Kumar");
 
 
 let personOne ={
-    name:'ranjith kumar',
+    name:'Ranjith Kumar',
     age:'28'
-};
+}
+console.log(personOne.name); */
+
+
+//JSON normal object format
+let object1={
+    name:'X',
+    age:'20',
+    DOB:'5/10/1995'
+}
+console.log(typeof object1);
+
+// conver from normal object to JSON STRING FORMAT 
+let stringifiedObject =JSON.stringify(object1);
+console.log(typeof stringifiedObject);
+
+
+//Again convert from JSON string to normal object format
+let parseObject=JSON.parse(stringifiedObject);
+console.log(typeof parseObject);
